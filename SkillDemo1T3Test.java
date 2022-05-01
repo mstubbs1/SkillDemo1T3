5548e0accf76ed2f695e41983bf5926a4b5a3be3
mstubbs1 <103288275+mstubbs1@users.noreply.github.com>
2022-05-01 15:41:37 -0700

    Added the files

---
+++ SkillDemo1T3Test.java
@@ -0,0 +1,11 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class SkillDemo1T3Test {
+
+	@Test
+	public void subtraction(){
+		assertEquals(1,SkillDemo1T3.subtract(5,2));
+
+	}
+}
